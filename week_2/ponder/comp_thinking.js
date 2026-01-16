c548a5248c695d3fb71ee6e826b679344f2dfca7
Sarah Penfold <sarahpenfold07@gmail.com>
2026-01-15 17:45:01 -0700

    comp_thinking js changes

---
+++ week_2/ponder/comp_thinking.js
@@ -0,0 +1,27 @@
+
+let selectElem = document.querySelector('#theme-select');
+let pageContent = document.querySelector('body');
+let boxSize = document.getElementById('logo');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current === 'ocean') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
+        pageContent.style.fontFamily = "Papyrus, fantasy";
+        boxSize.style.transform = 'scale(0, 0)'
+    } else if (current === 'forest') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
+        pageContent.style.fontFamily = "Impact, sans-serif";
+        boxSize.style.transform = 'scale(0, 0)'
+    } else if (current === 'desert') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
+        pageContent.style.fontFamily = "'Big Caslon', serif";
+        boxSize.style.transform = 'scale(0, 0)'
+    } else {
+        document.body.style.backgroundImage = "none";
+        pageContent.style.fontFamily = "Georgia, serif";
+        boxSize.style.transform = 'scale(1, 1)'
+    }
+} 
--- week_2/ponder/demo.js
+++
@@ -1,4 +0,0 @@
-//name = name   scope = global   value = Sarah 
-// use let or const
-
-let name = 'Sarah';
