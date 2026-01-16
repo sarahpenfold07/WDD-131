09bec1d354d74d981f1e2cc147bfbefb321fbcec
Sarah Penfold <sarahpenfold07@gmail.com>
2026-01-15 20:23:22 -0700

    added home page img

--- week_2/ponder/comp_thinking.js
+++ week_2/ponder/comp_thinking.js
@@ -9,15 +9,16 @@ function changeTheme() {
     let current = selectElem.value;
     if (current === 'shire') {
         document.body.style.backgroundImage = "url('https://sarahpenfold07.github.io/WDD-131/week_2/images/Shire.avif')";
-        pageContent.style.color = "white"
+        pageContent.style.color = "#fff"
         pageContent.style.fontFamily = "'Big Caslon', serif";
         boxSize.style.transform = 'scale(0, 0)'
-    } else if (current === 'forest') {
-        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
+    } else if (current === 'tatooine') {
+        document.body.style.backgroundImage = "url('https://sarahpenfold07.github.io/WDD-131/week_2/images/Star_Wars.jpg')";
         pageContent.style.fontFamily = "Impact, sans-serif";
         boxSize.style.transform = 'scale(0, 0)'
-    } else if (current === 'desert') {
-        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
+    } else if (current === 'hogwartz') {
+        document.body.style.backgroundImage = "url('https://sarahpenfold07.github.io/WDD-131/week_2/images/Harry_Potter.jpg')";
+        pageContent.style.color = "white"
         pageContent.style.fontFamily = "'Big Caslon', serif";
         boxSize.style.transform = 'scale(0, 0)'
     } else {
