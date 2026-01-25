d9bc2575e75c6957153beb16d026efe371036bcf
Sarah Penfold <sarahpenfold07@gmail.com>
2026-01-24 19:53:09 -0700

    fixed nav for laptop size

--- week_3/prove/cool_pics.js
+++ week_3/prove/cool_pics.js
@@ -5,8 +5,3 @@ showList.addEventListener('click', () => {
     nav.classList.toggle('active');
 })
 
-window.addEventListener('resize', () => {
-    if(window.innerWidth >= 700) {
-        nav.classList.remove('active');
-    }
-})
