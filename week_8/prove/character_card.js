11ee71b9a14b81ce29ef845a3303b17ed14370ac
Sarah Penfold <sarahpenfold07@gmail.com>
2026-02-28 10:22:40 -0700

    character card

--- week_8/prove/character_card.js
+++ week_8/prove/character_card.js
@@ -1,4 +1,51 @@
+const character = {
+    code: 'CSE121b',
+    name: 'Javascript Language',
+    logo: 'images/js-logo.png',
+    information: [
+        {
+            class: 'Hylian Warrior',
+            level: 0,
+            health: 8
+        }
+    ]
+};
+    
+
+function displayInformation (characterInformation) {
+    return`<div>
+    <p><strong>Class:</strong> ${characterInformation.class}</p>
+    <p><strong>Level:</strong> ${characterInformation.level}</p>
+    <p><strong>Health:</strong> ${characterInformation.health}</p>
+    </div>
+    `
+}
+
+function renderInformation (information) {
+    const html = information.map(displayInformation);
+    document.querySelector("#info").innerHTML = html.join("")
+}
+
+renderInformation(character.information)
+
+document.querySelector("#attack").addEventListener("click", function () {
+    const linkHealth = character.information[0];
+
+    if (linkHealth.health > 0) {
+        linkHealth.health--;
+        renderInformation(character.information);
+    }
+
+    if (linkHealth.health === 0) {
+        document.querySelector("#gameOver").style.display = "block";
+        let modal = document.querySelector('dialog')
+        modal.showModal();
+    }
+});
+
+document.querySelector("#level").addEventListener("click", function() {
+    character.information[0].level++;
+    renderInformation(character.information);
+});
 
 
-newSectionForm.reset()
-renderSections(aCourse.sections);
